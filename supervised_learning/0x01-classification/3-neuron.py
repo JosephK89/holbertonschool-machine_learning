159e97c18224ccc4173b8483cd594e4c7bad1ad1
root <josephkamel262@gmail.com>
2021-08-08 16:16:28 +0000

    Add supervised_learning

---
+++ supervised_learning/0x01-classification/3-neuron.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+"""neuron class module"""
+import numpy as np
+
+
+class Neuron:
+    """Neuron class"""
+    def __init__(self, nx):
+        """Class Initialization"""
+        if type(nx) != int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        self.__W = np.random.randn(1, nx)
+        self.__b = 0
+        self.__A = 0
+
+    @property
+    def W(self):
+        """getter function for weights"""
+        return self.__W
+
+    @property
+    def b(self):
+        """getter function for biases"""
+        return self.__b
+
+    @property
+    def A(self):
+        """getter function for A"""
+        return self.__A
+
+    def forward_prop(self, X):
+        """forward propagation function"""
+        Z = np.matmul(self.__W, X) + self.__b
+        self.__A = 1 / (1 + np.exp(-Z))
+        return self.__A
+
+    def cost(self, Y, A):
+        """model cost function"""
+        cost_array = np.multiply(np.log(A), Y) + np.multiply((
+            1 - Y), np.log(1.0000001 - A))
+        cost = -np.sum(cost_array) / len(A[0])
+        return cost

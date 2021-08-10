e09b0e16886d9572d29d5d541d19559e8da4b1dd
JosephK89 <josephkamel262@gmail.com>
2021-08-10 17:24:23 +0000

    Add supervised_learning

--- supervised_learning/0x01-classification/27-deep_neural_network.py
+++ supervised_learning/0x01-classification/27-deep_neural_network.py
@@ -10,14 +10,14 @@ class DeepNeuralNetwork:
     def __init__(self, nx, layers):
         """Data initialization"""
         if type(nx) != int:
-            raise TypeError("nx must be an integer")
+            raise TypeError("nx must be an integers")
         if nx < 1:
-            raise ValueError("nx must be a positive integer")
+            raise ValueError("nx must be a positive integers")
         if type(layers) != list or len(layers) == 0:
             raise TypeError("layers must be a list of positive integers")
-        self.L = len(layers)
-        self.cache = {}
-        self.weights = {}
+        self.__L = len(layers)
+        self.__cache = {}
+        self.__weights = {}
         layer = 1
         layer_size = nx
         for i in layers:
@@ -25,9 +25,9 @@ class DeepNeuralNetwork:
                 raise TypeError("layers must be a list of positive integers")
             w = "W" + str(layer)
             b = "b" + str(layer)
-            self.weights[w] = np.random.randn(
+            self.__weights[w] = np.random.randn(
                 i, layer_size) * np.sqrt(2/layer_size)
-            self.weights[b] = np.zeros((i, 1))
+            self.__weights[b] = np.zeros((i, 1))
             layer += 1
             layer_size = i
 
